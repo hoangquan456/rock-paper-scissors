b7f29cc8266e9196a5a3100f796838a254fe8973
Quan Truong <truonghoangquan456@gmail.com>
2025-03-02 01:49:40 -0500

    Add UI and Improve logic of the game
    I add some button to the UI. Also, the logic has been fixed and now
    player can play again and again if they want.

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 function getComputerChoice() {
-    let rand = Math.random() * 3;
-    if (rand < 1) return "rock";
-    if (rand < 2) return "paper";
+    let rand = Math.random();
+    if (rand < 1/3) return "rock";
+    if (rand < 2/3) return "paper";
     return "scissors";
-}
-
-
-function getHumanChoice() {
-    let decision = prompt("Make your decision (R)ock, (P)aper, (S)cissors").toUpperCase();
-    if (decision == "R") return "rock";
-    else if (decision == "P") return "paper";
-    else if (decision == "S") return "scissors";
-    else {
-        alert("your decision is invalid");
-        return getHumanChoice();
-    }
 }
 
 function announce(announcement, c1, c2) {
@@ -22,32 +10,53 @@ function announce(announcement, c1, c2) {
         ++computerScore;
     else 
         ++humanScore;
-    alert(`You ${announcement}! ${c1} beats ${c2}`);
+    roundResult.textContent = `You ${announcement}! ${c1} beats ${c2}`;
 }
 
 function playRound(human, computer) {
     if (human == computer) {
-        alert("You draw!");
+        roundResult.textContent = "You draw!";
     }
     else {
         if ((human == "rock" && computer == "paper") || (human == "paper" && computer == "scissors") || (human == "scissors" && computer == "rock"))
             announce("lose", computer, human);
         else 
-        announce("win", human, computer); 
+            announce("win", human, computer); 
     }
+    setScore();
 }
 let humanScore = 0, computerScore = 0;
 
-function playGame() {
-    for(let i = 0; i < 5; ++i) {
-        playRound(getHumanChoice(), getComputerChoice());
+let body = document.querySelector("body");
+body.addEventListener("click", (e)=>{
+    if (e.target.tagName !== "BUTTON") return;
+    playRound(e.target.id, getComputerChoice());
+    if (humanScore == 5) {
+        setTimeout(() => {
+            alert("Victory! You have beaten AI!");
+            humanScore = computerScore = 0;
+            roundResult.textContent = "";
+            setScore();
+        }, 0);
     }
-    if (humanScore > computerScore) 
-        alert("You win the game");
-    else 
-        alert("You lose the game");
+    else if (computerScore == 5) {
+        setTimeout(() => {
+            alert("Defeat! AI will take over the world")
+            humanScore = computerScore = 0;
+            roundResult.textContent = "";
+            setScore();
+        }, 0);
+    }
+});
+
+let div1 = document.createElement("div"); 
+let div2 = document.createElement("div"); 
+let roundResult = document.createElement("div");
+body.append(div1, div2, roundResult);
+
+function setScore() {
+    div1.textContent = `Your Score: ${humanScore}`;
+    div2.textContent = `Computer Score: ${computerScore}`;
 }
 
-playGame();
-
-// console.log(getHumanChoice());
+setScore();
